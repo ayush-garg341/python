50d5011ab1661232ece5f17571c14a8b61850dd2
Ayush Garg <ayush.garg@innovaccer.com>
2021-09-27 17:19:42 +0530

    composition and aggregation examples done

---
+++ oops/aggregation.py
@@ -0,0 +1,45 @@
+# Form of association
+
+"""
+Aggregation follows the Has-A model. 
+This creates a parent-child relationship between two classes, with one class owning the object of another.
+
+In aggregation, the lifetime of the owned object does not depend on the lifetime of the owner.
+The owner object could get deleted, but the owned object can continue to exist in the program.
+In aggregation, the parent only contains a reference to the child, which removes the child's dependency.
+"""
+
+
+# Each person is associated with a country, but the country can exist without that person.
+
+class Country:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def printDetails(self):
+        print("Country Name:", self.name)
+        print("Country Population", self.population)
+
+
+
+class Person:
+
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+    def printDetails(self):
+        print("Person name: ", self.name)
+        self.country.printDetails()
+
+
+c = Country("India", 125000000)
+p = Person("Ayush Garg", c)
+
+p.printDetails()
+
+del p
+
+print("After deleting person object, country can still exist")
+c.printDetails()
---
+++ oops/composition.py
@@ -0,0 +1,52 @@
+"""
+Composition is the practice of accessing other class objects in your class. 
+In such a scenario, the class which creates the object of the other class is known as the owner and is responsible for the lifetime of that object.
+
+Composition relationships are Part-of relationships where the part must constitute a segment of the whole object.
+In composition, the lifetime of the owned object depends on the lifetime of the owner.
+"""
+
+# A car is composed of an engine, tires, and doors. 
+# In this case, a Car owned these objects, so a Car is an Owner class, and the tires, doors, and engine classes are Owned classes.
+
+class Engine:
+    def __init__(self, engine):
+        self.capacity = engine
+
+    def printDetails(self):
+        print("Engine capacity : ", self.capacity)
+
+
+class Doors:
+    def __init__(self, doors):
+        self.doors = doors
+
+    def printDetails(self):
+        print("Number of doors : ", self.doors)
+
+
+class Tires:
+    def __init__(self, tires):
+        self.tires = tires
+
+    def printDetails(self):
+        print("Number of tires : ", self.tires)
+
+
+class Car:
+    def __init__(self, eng, doors, tires, color):
+        self.engine = Engine(eng)
+        self.doors = Doors(doors)
+        self.tires = Tires(tires)
+        self.color = color
+
+    def printDetails(self):
+        self.engine.printDetails()
+        self.doors.printDetails()
+        self.tires.printDetails()
+        print("Color of car is : ", self.color)
+
+
+car = Car(1500, 3, 4, "Black")
+
+car.printDetails()
