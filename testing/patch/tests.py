989cee4410a3d2a4d20e432a83df2ca037f69b5d
Ayush Garg <ayush.garg@innovaccer.com>
2021-10-05 18:45:31 +0530

    pathcing and mocing API and other attributes seen

---
+++ testing/patch/tests.py
@@ -0,0 +1,41 @@
+import unittest
+from my_calendar import get_holidays, requests
+from requests.exceptions import Timeout
+from unittest.mock import patch
+
+# class TestCalendar(unittest.TestCase):
+#     @patch('my_calendar.requests')
+#     def test_get_holidays_timeout(self, mock_requests):
+#             mock_requests.get.side_effect = Timeout
+#             with self.assertRaises(Timeout):
+#                 get_holidays()
+#                 mock_requests.get.assert_called_once()
+
+
+"""
+    Patch as a context manager.
+    1. You only want to mock an object for a part of the test scope.
+    2. You are already using too many decorators or parameters, which hurts your test’s readability.
+"""
+
+
+
+# class TestCalendar(unittest.TestCase):
+#     def test_get_holidays_timeout(self):
+#         with patch('my_calendar.requests') as mock_requests:
+#             mock_requests.get.side_effect = Timeout
+#             with self.assertRaises(Timeout):
+#                 get_holidays()
+#                 mock_requests.get.assert_called_once()
+
+
+
+class TestCalendar(unittest.TestCase):
+    @patch.object(requests, 'get', side_effect=requests.exceptions.Timeout)
+    def test_get_holidays_timeout(self, mock_requests):
+            with self.assertRaises(requests.exceptions.Timeout):
+                get_holidays()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testing/test_calc.py
@@ -0,0 +1,67 @@
+"""
+    https://realpython.com/python-mock-library
+"""
+
+import calc
+import unittest
+
+class TestCalc(unittest.TestCase):
+
+    # Any test case must begin with test_ else it wont be considered as a test
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        print("Runs only once before test cases begins")
+
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print("Runs only once after all test cases finish running")
+    
+
+    def setUp(self) -> None:
+        """This fun. runs before running each test case."""
+        print("set up before each test case")
+
+    def tearDown(self) -> None:
+        """This fun. runs after each test case finsh running."""
+        print("tear down after each test case")
+
+    def test_add(self):
+
+        self.assertEqual(calc.add(2, 3), 5)
+        self.assertEqual(calc.add(-1, 3), 2)
+        self.assertEqual(calc.add(-2, -3), -5)
+
+    
+    def test_substract(self):
+
+        self.assertEqual(calc.subtract(5, 3), 2)
+        self.assertEqual(calc.subtract(-1, 3), -4)
+        self.assertEqual(calc.subtract(-2, -3), 1)
+        self.assertEqual(calc.subtract(2, 3), -1)
+
+    
+    def test_multiply(self):
+
+        self.assertEqual(calc.multiply(2, 3), 6)
+        self.assertEqual(calc.multiply(-1, 3), -3)
+        self.assertEqual(calc.multiply(-5, -3), 15)
+
+
+    def test_divide(self):
+
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-3, 1), -3)
+        self.assertEqual(calc.divide(-3, -3), 1)
+        self.assertEqual(calc.divide(5, 2), 2.5)
+
+        self.assertRaises(ValueError, calc.divide, 10, 0)
+        # self.assertRaises(ValueError, calc.divide, 10, 2)  # test case failed here, since ValueError is not raised by divide
+
+        with self.assertRaises(ValueError):    
+            calc.divide(10, 0)   # value error will be rasied here.
+
+
+if __name__ == "__main__":
+    unittest.main()
