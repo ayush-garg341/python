01943fbe0759f244994ae4499ded095f12370c12
Ayush Garg <ayush.garg@innovaccer.com>
2022-02-12 10:40:53 +0530

    count subset sum and target sum done

---
+++ data_structures/dp/zero_one/count_subset_sum.py
@@ -0,0 +1,109 @@
+"""
+Given a set of positive numbers, find the total number of subsets whose sum is equal to a given number ‘S’.
+
+example-1:
+    Input: {1, 1, 2, 3}, S=4
+    Output: 3
+    The given set has '3' subsets whose sum is '4': {1, 1, 2}, {1, 3}, {1, 3}
+    Note that we have two similar sets {1, 3}, because we have two '1' in our input.
+
+example-2:
+    Input: {1, 2, 7, 1, 5}, S=9
+    Output: 3
+    The given set has '3' subsets whose sum is '9': {2, 7}, {1, 7, 1}, {1, 2, 1, 5}
+"""
+
+
+def count_subset_sum(arr, s):
+    count = 0
+    current_idx = 0
+    count = count_subset_sum_brute_force(arr, current_idx, s)
+    return count
+
+
+def count_subset_sum_brute_force(arr: list, current_idx: int, s: int) -> int:
+    if s == 0:
+        return 1
+    if current_idx >= len(arr):
+        return 0
+
+    count = 0
+    if arr[current_idx] <= s:
+        count += count_subset_sum_brute_force(arr, current_idx+1, s-arr[current_idx])
+
+    count += count_subset_sum_brute_force(arr, current_idx+1, s)
+
+    return count
+
+
+print("=============== brute force recursive ===============")
+print(count_subset_sum([1, 1, 2, 3], 4))
+print(count_subset_sum([1, 2, 7, 1, 5], 9))
+print(count_subset_sum([1, 2, 3], 4))
+
+
+
+def count_subset_sum_dp(arr, s):
+    count = 0
+    current_idx = 0
+    dp = [[-1 for i in range(s+1)] for j in range(len(arr))]
+    count = count_subset_sum_dp_top_bottom(arr, current_idx, s, dp)
+    return count
+
+
+def count_subset_sum_dp_top_bottom(arr: list, current_idx: int, s: int, dp: list) -> int:
+    if s == 0:
+        return 1
+    if current_idx >= len(arr):
+        return 0
+
+    if dp[current_idx][s] == -1:
+        count = 0
+        if arr[current_idx] <= s:
+            count += count_subset_sum_brute_force(arr, current_idx+1, s-arr[current_idx])
+
+        count += count_subset_sum_brute_force(arr, current_idx+1, s)
+        dp[current_idx][s] = count
+
+    return dp[current_idx][s]
+
+
+print("=============== top bottom dp recursive ===============")
+print(count_subset_sum_dp([1, 1, 2, 3], 4))
+print(count_subset_sum_dp([1, 2, 7, 1, 5], 9))
+print(count_subset_sum_dp([1, 2, 3], 4))
+
+
+def count_subset_sum_dp_bottom_up_tabular(arr: list, s: int) -> int:
+    n = len(arr)
+    if n == 0:
+        return 0
+
+    dp = [[0 for i in range(s+1)] for j in range(len(arr))]
+
+    for i in range(len(arr)):
+        dp[i][0] = 1
+
+    for s in range(1, s+1):
+        if s == arr[0]:
+            dp[0][s] = 1
+
+    for i in range(1, len(arr)):
+        for s in range(1, s+1):
+            count1, count2 = 0, 0
+            count1 = dp[i-1][s]
+            if arr[i] <= s:
+                count2 = dp[i-1][s-arr[i]]
+
+            dp[i][s] = count1 + count2
+
+
+    return dp[n-1][s]
+
+
+
+
+print("=============== bottom up tabular ===============")
+print(count_subset_sum_dp_bottom_up_tabular([1, 1, 2, 3], 4))
+print(count_subset_sum_dp_bottom_up_tabular([1, 2, 7, 1, 5], 9))
+print(count_subset_sum_dp_bottom_up_tabular([1, 2, 3], 4))
---
+++ data_structures/dp/zero_one/target_sum.py
@@ -0,0 +1,99 @@
+"""
+Given a set of positive numbers (non zero) and a target sum ‘S’.
+Each number should be assigned either a ‘+’ or ‘-’ sign.
+We need to find out total ways to assign symbols to make the sum of numbers equal to target ‘S’.
+
+example-1:
+    Input: {1, 1, 2, 3}, S=1
+    Output: 3
+    Explanation: The given set has '3' ways to make a sum of '1': {+1-1-2+3} & {-1+1-2+3} & {+1+1+2-3}
+
+example-2:
+    Input: {1, 2, 7, 1}, S=9
+    Output: 2
+    Explanation: The given set has '2' ways to make a sum of '9': {+1+2+7-1} & {-1+2+7+1}
+
+Explanation: Need to find two subsets s.t S1-S2 = s
+            We already know S1 + S2 = S
+            Adding both equations:
+                2*S1 = S + s
+                S1 = (S + s)/2
+"""
+
+
+def find_target_subsets(arr, s):
+    if any(i < 1 for i in arr):
+        return -1 #invalid input, the problem expects only positive numbers
+
+    # if 's + totalSum' is odd, we can't find a subset with sum equal to '(s + totalSum) / 2'
+    total_sum = sum(arr)
+    if total_sum < s or (s + total_sum) %2 ==1:
+        return 0
+    current_idx = 0
+    return find_target_subsets_rec_brute_force(arr, current_idx, (s + total_sum)/2)
+
+
+def find_target_subsets_rec_brute_force(arr: list, current_idx: int, s: int) -> int:
+
+    if s == 0:
+        return 1
+
+    if current_idx >= len(arr):
+        return 0
+
+    count = 0
+    if arr[current_idx] <= s:
+        count += find_target_subsets_rec_brute_force(arr, current_idx+1, s-arr[current_idx])
+
+    count += find_target_subsets_rec_brute_force(arr, current_idx+1, s)
+
+    return count
+
+
+print("=================== brute force recursive ============")
+print(find_target_subsets([1, 1, 2, 3], 1))
+print(find_target_subsets([1, 2, 7, 1], 9))
+print(find_target_subsets([1, 2, 7, 1], 11))
+
+
+def find_target_subsets_dp(arr, s):
+    if any(i < 1 for i in arr):
+        return -1
+
+    total_sum = sum(arr)
+    if total_sum < s or (s + total_sum) % 2 ==1:
+        return 0
+    desired_sum = (s+total_sum)/2
+    current_idx = 0
+    dp = [[0 for i in range(desired_sum+1)] for j in range(len(arr))]
+    return find_target_subsets_dp_top_bottom(arr, current_idx, desired_sum, dp)
+
+
+
+def find_target_subsets_dp_top_bottom(arr: list, current_idx: int, s: int, dp: list):
+    if s == 0:
+        return 1
+
+    if current_idx >= len(arr):
+        return 0
+
+    if dp[current_idx][s] == -1:
+        count = 0
+        if arr[current_idx] <= s:
+            count += find_target_subsets_dp_top_bottom(arr, current_idx+1, s-arr[current_idx], dp)
+
+        count += find_target_subsets_dp_top_bottom(arr, current_idx+1, s, dp)
+
+        dp[current_idx][s] = count
+
+    return dp[current_idx][s]
+
+
+print("=================== top bottom dp recursive ============")
+print(find_target_subsets([1, 1, 2, 3], 1))
+print(find_target_subsets([1, 2, 7, 1], 9))
+print(find_target_subsets([1, 2, 7, 1], 11))
+
+
+
+# exact same solution will work for tabular approach as it worked in count subset sum 
