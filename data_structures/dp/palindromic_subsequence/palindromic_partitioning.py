805617bc7ce5976e9347f2be246458b0f1011975
Ayush Garg <ayush.garg@innovaccer.com>
2022-03-08 09:59:33 +0530

    palindrmoic partition brute force and top bottom done

--- data_structures/dp/palindromic_subsequence/palindromic_partitioning.py
+++ data_structures/dp/palindromic_subsequence/palindromic_partitioning.py
@@ -19,51 +19,98 @@ example3:
 """
 
 
-# def count_palindromic_cuts_dp_top_bottom(string):
-# n = len(string)
-# if n == 0:
-# return -1
-# dp = [[0 for i in range(n)] for j in range(n)]
-
-# start = 0
-# end = len(string) - 1
-# count = count_palindromic_cuts_dp_top_bottom_recursive(string, dp, start, end)
-# return count
+def count_palindromic_recursive(string):
+    n = len(string)
+    if n == 0:
+        return -1
+    start = 0
+    end = len(string) - 1
+    count = count_palindromic_recursive_brute_force(string, start, end)
+    return count
 
 
-# def count_palindromic_cuts_dp_top_bottom_recursive(
-# string: str, dp: list, start: int, end: int
-# ):
-# if end - start - 1 == 0 and string[start] != string[end]:
-# return 1
+def count_palindromic_recursive_brute_force(string: str, start: int, end: int):
+    if start >= end:
+        return 0
 
-# if start >= end:
-# return 0
+    print("start, end = ", start, end)
+    if string[start] == string[end]:
+        remaining_len = 0
+        if remaining_len == count_palindromic_recursive_brute_force(
+            string, start + 1, end - 1
+        ):
+            return 0
+        else:
+            count1 = count_palindromic_recursive_brute_force(string, start + 1, end)
+            count2 = count_palindromic_recursive_brute_force(string, start, end - 1)
+            count = 1 + min(count1, count2)
+    else:
+        count1 = count_palindromic_recursive_brute_force(string, start + 1, end)
+        count2 = count_palindromic_recursive_brute_force(string, start, end - 1)
+        count = 1 + min(count1, count2)
 
-# if dp[start][end] == 0:
-# if string[start] == string[end]:
-# remaining_len = end - start - 1
-# if remaining_len == count_palindromic_cuts_dp_top_bottom_recursive(
-# string, dp, start + 1, end - 1
-# ):
-# dp[start][end] = 0
-
-# count1 = count_palindromic_cuts_dp_top_bottom_recursive(
-# string, dp, start + 1, end
-# )
-# count2 = count_palindromic_cuts_dp_top_bottom_recursive(
-# string, dp, start, end - 1
-# )
-# dp[start][end] = min(count1, count2)
-
-# return dp[start][end]
+    return count
 
 
-# print("========= count palindrome cuts dp top bottom recursive =======")
-# # print(count_palindromic_cuts_dp_top_bottom("abdbca"))
-# # print(count_palindromic_cuts_dp_top_bottom("cddpd"))
-# print(count_palindromic_cuts_dp_top_bottom("pqr"))
-# print(count_palindromic_cuts_dp_top_bottom("pp"))
+print("========= count palindrome cuts brute force recursive =======")
+print(count_palindromic_recursive("abdbca"))
+print(count_palindromic_recursive("cddpd"))
+print(count_palindromic_recursive("pqr"))
+print(count_palindromic_recursive("pp"))
+
+
+def count_palindromic_cuts_dp_top_bottom(string):
+    n = len(string)
+    if n == 0:
+        return -1
+    dp = [[-1 for i in range(n)] for j in range(n)]
+
+    start = 0
+    end = len(string) - 1
+    count = count_palindromic_cuts_dp_top_bottom_recursive(string, dp, start, end)
+    return count
+
+
+def count_palindromic_cuts_dp_top_bottom_recursive(
+    string: str, dp: list, start: int, end: int
+):
+
+    if start >= end:
+        return 0
+
+    if dp[start][end] == -1:
+        print("start, end = ", start, end)
+        if string[start] == string[end]:
+            remaining_len = 0
+            if remaining_len == count_palindromic_cuts_dp_top_bottom_recursive(
+                string, dp, start + 1, end - 1
+            ):
+                dp[start][end] = 0
+            else:
+                count1 = count_palindromic_cuts_dp_top_bottom_recursive(
+                    string, dp, start + 1, end
+                )
+                count2 = count_palindromic_cuts_dp_top_bottom_recursive(
+                    string, dp, start, end - 1
+                )
+                dp[start][end] = 1 + min(count1, count2)
+        else:
+            count1 = count_palindromic_cuts_dp_top_bottom_recursive(
+                string, dp, start + 1, end
+            )
+            count2 = count_palindromic_cuts_dp_top_bottom_recursive(
+                string, dp, start, end - 1
+            )
+            dp[start][end] = 1 + min(count1, count2)
+
+    return dp[start][end]
+
+
+print("========= count palindrome cuts dp top bottom recursive =======")
+print(count_palindromic_cuts_dp_top_bottom("abdbca"))
+print(count_palindromic_cuts_dp_top_bottom("cddpd"))
+print(count_palindromic_cuts_dp_top_bottom("pqr"))
+print(count_palindromic_cuts_dp_top_bottom("pp"))
 
 
 def count_palindromic_cuts_dp_bottom_up_tabular(string):
