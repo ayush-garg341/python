7a2e5ce5fbe47a21abbcac5317f12308c65581cf
Ayush Garg <ayush.garg@innovaccer.com>
2022-03-17 15:42:42 +0530

    max sum inc subseq brute force recursive top bottom

--- data_structures/dp/longest_common/max_sum_inc_subseq.py
+++ data_structures/dp/longest_common/max_sum_inc_subseq.py
@@ -16,6 +16,57 @@ example2:
 import math
 
 
+def max_sum_inc_subseq_brute_force(seq):
+    prev = -1
+    curr = 0
+    return max_sum_inc_subseq_brute_force_recursive(seq, prev, curr)
+
+
+def max_sum_inc_subseq_brute_force_recursive(seq: list, prev: int, curr: int):
+    if prev == curr or curr == len(seq):
+        return 0
+
+    sum1, sum2 = 0, 0
+    if prev == -1 or seq[curr] > seq[prev]:
+        sum1 = seq[curr] + max_sum_inc_subseq_brute_force_recursive(seq, curr, curr + 1)
+    sum2 = max_sum_inc_subseq_brute_force_recursive(seq, prev, curr + 1)
+    return max(sum1, sum2)
+
+
+print("====== max sum for increasing subseq brute force recursive ======")
+print(max_sum_inc_subseq_brute_force([4, 1, 2, 6, 10, 1, 12]))
+print(max_sum_inc_subseq_brute_force([-4, 10, 3, 7, 15]))
+
+
+def max_sum_inc_subseq_dp_top_bottom(seq):
+    dp = [[-math.inf for _ in range(len(seq))] for _ in range(len(seq))]
+    prev = -1
+    curr = 0
+    return max_sum_inc_subseq_dp_top_bottom_recursive(seq, dp, prev, curr)
+
+
+def max_sum_inc_subseq_dp_top_bottom_recursive(
+    seq: list, dp: list, prev: int, curr: int
+):
+    if prev == curr or curr == len(seq):
+        return 0
+
+    if dp[prev + 1][curr] == -math.inf:
+        sum1, sum2 = 0, 0
+        if prev == -1 or seq[curr] > seq[prev]:
+            sum1 = seq[curr] + max_sum_inc_subseq_dp_top_bottom_recursive(
+                seq, dp, curr, curr + 1
+            )
+        sum2 = max_sum_inc_subseq_dp_top_bottom_recursive(seq, dp, prev, curr + 1)
+        dp[prev + 1][curr] = max(sum1, sum2)
+    return dp[prev + 1][curr]
+
+
+print("====== max sum for increasing subseq dp top bottom recursive ======")
+print(max_sum_inc_subseq_dp_top_bottom([4, 1, 2, 6, 10, 1, 12]))
+print(max_sum_inc_subseq_dp_top_bottom([-4, 10, 3, 7, 15]))
+
+
 def max_sum_inc_subseq_dp_bottom_up_tabular(seq):
     n = len(seq)
     if n == 0:
