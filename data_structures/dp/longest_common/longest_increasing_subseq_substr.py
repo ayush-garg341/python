455f5c7f2aef94f80817a100fd06e3bbe082688e
Ayush Garg <ayush.garg@innovaccer.com>
2022-03-17 13:30:01 +0530

    brute force and recursive lis

--- data_structures/dp/longest_common/longest_increasing_subseq_substr.py
+++ data_structures/dp/longest_common/longest_increasing_subseq_substr.py
@@ -39,6 +39,63 @@ print(longest_increasing_substr_naive([4, 2, 3, 6, 10, 1, 12]))
 print(longest_increasing_substr_naive([-4, 10, 3, 7, 15]))
 
 
+def longest_common_subseq_brute_force(seq):
+    prev = -1
+    curr = 0
+    return longest_common_subseq_brute_force_recursive(seq, prev, curr)
+
+
+def longest_common_subseq_brute_force_recursive(seq: list, prev: int, curr: int):
+    if prev == curr or curr == len(seq):
+        return 0
+
+    c1, c2 = 0, 0
+    if prev == -1 or seq[curr] > seq[prev]:
+        c1 = 1 + longest_common_subseq_brute_force_recursive(seq, curr, curr + 1)
+
+    c2 = longest_common_subseq_brute_force_recursive(seq, prev, curr + 1)
+
+    return max(c1, c2)
+
+
+print("====== longest increasing subseq burte force recursive ========")
+print(longest_common_subseq_brute_force([4, 2, 3, 6, 10, 1, 12]))
+print(longest_common_subseq_brute_force([-4, 10, 3, 7, 15]))
+print(longest_common_subseq_brute_force([8, 9, 9, 10, 12, 21, 20]))
+
+
+def longest_common_subseq_dp_top_bottom(seq):
+    dp = [[0 for _ in range(len(seq))] for _ in range(len(seq))]
+    prev = -1
+    curr = 0
+    return longest_common_subseq_dp_top_bottom_recursive(seq, dp, prev, curr)
+
+
+def longest_common_subseq_dp_top_bottom_recursive(
+    seq: list, dp: list, prev: int, curr: int
+):
+    if prev == curr or curr == len(seq):
+        return 0
+
+    if dp[prev + 1][curr] == 0:
+        c1, c2 = 0, 0
+        if prev == -1 or seq[curr] > seq[prev]:
+            c1 = 1 + longest_common_subseq_dp_top_bottom_recursive(
+                seq, dp, curr, curr + 1
+            )
+        c2 = longest_common_subseq_dp_top_bottom_recursive(seq, dp, prev, curr + 1)
+        dp[prev + 1][curr] = max(c1, c2)
+
+    return dp[prev + 1][curr]
+
+
+print("====== longest increasing subseq dp top bottom recursive ========")
+print(longest_common_subseq_dp_top_bottom([4, 2, 3, 6, 10, 1, 12]))
+print(longest_common_subseq_dp_top_bottom([-4, 10, 3, 7, 15]))
+print(longest_common_subseq_dp_top_bottom([8, 9, 9, 10, 12, 21, 20]))
+print(longest_common_subseq_dp_top_bottom([1, 2, 3, 4, 5, 6, 7, 8]))
+
+
 def longest_increasing_subseq_dp_bottom_up_tabular(seq):
     n = len(seq)
     if n == 0:
