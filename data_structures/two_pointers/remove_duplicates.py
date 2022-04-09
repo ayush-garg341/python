f07681fd5307c04308aa501ca7eec9b863e9fd0f
Ayush Garg <ayush.garg@innovaccer.com>
2022-04-09 11:04:49 +0530

    remove duplicates from array in single pass

---
+++ data_structures/two_pointers/remove_duplicates.py
@@ -0,0 +1,33 @@
+"""
+Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing
+the duplicates in-place return the length of the subarray that has no duplicate in it.
+
+example1:
+    Input: [2, 3, 3, 3, 6, 9, 9]
+    Output: 4
+    Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+
+example2:
+    Input: [2, 2, 2, 11]
+    Output: 2
+    Explanation: The first two elements after removing the duplicates will be [2, 11].
+"""
+
+
+def remove_duplicates(arr):
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
+    next_num = 0
+
+    for next_non_dup in range(1, len(arr)):
+        if arr[next_non_dup] == arr[next_num]:
+            next_non_dup += 1
+        else:
+            next_num += 1
+            arr[next_num] = arr[next_non_dup]
+
+    return arr[0 : next_num + 1]
+
+
+print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+print(remove_duplicates([2, 2, 2, 11]))
