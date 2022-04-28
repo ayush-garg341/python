33d2e8dbd5ca14fb67e8177e3c1139b887bcf5c1
Ayush Garg <ayush.garg@innovaccer.com>
2022-04-28 14:04:32 +0530

    max depth of bnary tree

--- data_structures/breadth_first_search/min_depth.py
+++ data_structures/breadth_first_search/min_depth.py
@@ -32,6 +32,26 @@ def find_minimum_depth(root):
     return min_depth
 
 
+def find_max_depth(root):
+    if root is None:
+        return -1
+    min_depth = 0
+    deq = []
+    deq.append(root)
+    while len(deq):
+        size = len(deq)
+        min_depth += 1
+        while size != 0:
+            pop = deq.pop(0)
+            size -= 1
+            if pop.left:
+                deq.append(pop.left)
+            if pop.right:
+                deq.append(pop.right)
+
+    return min_depth
+
+
 def main():
     root = TreeNode(12)
     root.left = TreeNode(7)
@@ -39,9 +59,11 @@ def main():
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
     print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+    print("Tree Max Depth: " + str(find_max_depth(root)))
     root.left.left = TreeNode(9)
     root.right.left.left = TreeNode(11)
     print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+    print("Tree Max Depth: " + str(find_max_depth(root)))
 
 
 main()
