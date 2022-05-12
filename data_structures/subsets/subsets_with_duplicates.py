4db0a2469a0ff3fb8dc5a26505ed66791379bd73
Ayush Garg <ayush.garg@innovaccer.com>
2022-05-12 15:29:51 +0530

    unique subsets with efficient approach done

--- data_structures/subsets/subsets_with_duplicates.py
+++ data_structures/subsets/subsets_with_duplicates.py
@@ -67,6 +67,26 @@ def find_subsets(nums):
     return subsets
 
 
+def find_subsets_efficient(nums):
+    subsets = []
+    end_index = 0
+    subsets.append([])
+    nums.sort()
+
+    for i in range(len(nums)):
+        start_index = 0
+        if i > 0 and nums[i] == nums[i - 1]:
+            start_index = end_index + 1
+        end_index = len(subsets) - 1
+
+        for j in range(start_index, end_index + 1):
+            new_set = list(subsets[j])
+            new_set.append(nums[i])
+            subsets.append(new_set)
+
+    return subsets
+
+
 def main():
 
     # print("Here is the list of subsets: " + str(find_subsets_naive_wrong([1, 3, 3])))
@@ -76,6 +96,12 @@ def main():
     print("Here is the list of subsets: " + str(find_subsets([1, 5, 3, 3])))
     print("Here is the list of subsets: " + str(find_subsets([1, 1])))
     print("Here is the list of subsets: " + str(find_subsets([1, 2, 2])))
+
+    print(" ========================= ")
+    print("Here is the list of subsets: " + str(find_subsets_efficient([1, 3, 3])))
+    print("Here is the list of subsets: " + str(find_subsets_efficient([1, 5, 3, 3])))
+    print("Here is the list of subsets: " + str(find_subsets_efficient([1, 1])))
+    print("Here is the list of subsets: " + str(find_subsets_efficient([1, 2, 2])))
 
 
 main()
