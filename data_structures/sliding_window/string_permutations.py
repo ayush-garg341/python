98e93a395be261ddad1aef2ec1e05872674eb317
Ayush Garg <ayush.garg@innovaccer.com>
2022-05-13 00:35:02 +0530

    string permutaion same as nums permutation

---
+++ data_structures/sliding_window/string_permutations.py
@@ -0,0 +1,12 @@
+"""
+This is same like generating permutations of numbers in a list.
+ex. nums = [ 1, 3, 5 ]
+[5, 1, 3]
+[1, 5, 3]
+[1, 3, 5]
+[5, 3, 1]
+[3, 5, 1]
+[3, 1, 5]
+
+it can be implemented using BFS/DFS the same approach as discussed in subsets/permutations.py
+"""
