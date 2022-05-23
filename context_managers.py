c2d1f86086430f8aebed3b0fb0125a58bed0f1a7
Ayush Garg <ayush.garg@innovaccer.com>
2022-05-23 12:14:38 +0530

    verified exit called on return from context manager

--- context_managers.py
+++ context_managers.py
@@ -1,6 +1,5 @@
 import os
 from contextlib import contextmanager
-from typing import final
 
 # f = open("sample.txt", 'w')
 # f.write("Adding some more text to it")
@@ -9,42 +8,53 @@ from typing import final
 
 # use of context manager "with", no need to write close, that get handled itself and hence efficiently managing resources.
 # Even if there is an error in writing or opening to it.
-with open("sample.txt", 'w') as f:
+with open("sample.txt", "w") as f:
     f.write("Adding some text to it")
 
-# class Open_file:
-#     def __init__(self, file_name, mode) -> None:
-#          self.file_name = file_name
-#          self.mode = mode
 
-#     def __enter__(self):
-#         self.file = open(self.file_name, self.mode)
-#         return self.file
+class Open_file:
+    def __init__(self, file_name, mode) -> None:
+        self.file_name = file_name
+        self.mode = mode
 
-#     def __exec__(self, exec_type, exec_val, traceback):
-#         self.file.close()
+    def __enter__(self):
+        print("Inside enter")
+        self.file = open(self.file_name, self.mode)
+        return self.file
+
+    def __exit__(self, exec_type, exec_val, traceback):
+        print("exit called")
+        self.file.close()
 
 
-# with Open_file("sample_2.txt", 'w') as f:   # init and enter method called
-#     f.write("I am Ayush working as backend developer (SDE-2) at innovaccer.")
+# Testing if exit is called when returning from with in the context decorator
+def random_fn():
+    print("inside fn")
+    with Open_file("sample_2.txt", "w") as f:  # init and enter method called
+        print("Opening a file")
+        f.write("I am Ayush working as backend developer (SDE-2) at innovaccer.")
+        return 6
 
-# # exec method called
 
-# print(f.closed)
+# # exit method called
+
+random_fn()
+print(f.closed)
 
 # Using contextlib
 print("---------------- Using contextLib ------------------ ")
 
+
 @contextmanager
 def open_file(file_name, mode):
     try:
-        f = open(file_name, mode) # class equivalent of init and enter method
-        yield f # yieldling out file object, so that we can work with it later
+        f = open(file_name, mode)  # class equivalent of init and enter method
+        yield f  # yieldling out file object, so that we can work with it later
     finally:
-        f.close() # exec method
+        f.close()  # exec method
 
 
-with open_file("sample_3.txt", 'w') as f:
+with open_file("sample_3.txt", "w") as f:
     f.write("Hey! you people are awesome")
 
 print(f.closed)
@@ -72,10 +82,11 @@ def change_dir(destination):
         os.chdir(destination)
         yield
     finally:
-        os.chdir(cwd) # we need not to do this again and again
+        os.chdir(cwd)  # we need not to do this again and again
+
 
 with change_dir("oops"):
-    print(os.listdir()) 
+    print(os.listdir())
 
 with change_dir("solid_principles"):
     print(os.listdir())
