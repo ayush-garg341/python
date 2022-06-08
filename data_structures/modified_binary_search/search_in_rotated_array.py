9cdb7d6689bfd4dcc01b9238dc2a1e56a680d9f3
Ayush Garg <ayush.garg@innovaccer.com>
2022-06-08 09:40:47 +0530

    search in rotated sorted array with duplicates

--- data_structures/modified_binary_search/search_in_rotated_array.py
+++ data_structures/modified_binary_search/search_in_rotated_array.py
@@ -38,12 +38,38 @@ def search_rotated_array(arr, key):
     return -1
 
 
+def search_rotated_array_with_duplicates(arr, key):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == key:
+            return mid
+        elif arr[start] == arr[mid] and arr[mid] == arr[end]:
+            start += 1
+            end -= 1
+        elif arr[mid] >= arr[start]:  # left part sorted in ascending order
+            if key < arr[mid] and key >= arr[start]:
+                end = mid - 1
+            else:  # key > arr[mid]
+                start = mid + 1
+        else:  # right part sorted in ascending order
+            if key > arr[mid] and key <= arr[end]:
+                start = mid + 1
+            else:  # key < arr[mid]
+                end = mid - 1
+
+    return -1
+
+
 def main():
     print(search_rotated_array([10, 15, 1, 3, 8], 15))
     print(search_rotated_array([4, 5, 7, 9, 10, -1, 2], 10))
     print(search_rotated_array([4, 5, 6, 7, 0, 1, 2], 0))
     print(search_rotated_array([4, 5, 6, 7, 0, 1, 2], 3))
     print(search_rotated_array([1], 0))
+    print(search_rotated_array_with_duplicates([2, 5, 6, 0, 0, 1, 2], 0))
+    print(search_rotated_array_with_duplicates([2, 5, 6, 0, 0, 1, 2], 3))
 
 
 main()
