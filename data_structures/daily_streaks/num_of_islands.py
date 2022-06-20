f34f1905070d71031c00382351fe479a98d96e92
Ayush Garg <ayush.garg@innovaccer.com>
2022-06-20 23:09:26 +0530

    num of islands using dfs in 2-d matrix done

---
+++ data_structures/daily_streaks/num_of_islands.py
@@ -0,0 +1,65 @@
+"""
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water)
+return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+example1:
+    Input: grid = [
+      ["1","1","1","1","0"],
+      ["1","1","0","1","0"],
+      ["1","1","0","0","0"],
+      ["0","0","0","0","0"]
+    ]
+    Output: 1
+
+example2:
+    Input: grid = [
+      ["1","1","0","0","0"],
+      ["1","1","0","0","0"],
+      ["0","0","1","0","0"],
+      ["0","0","0","1","1"]
+    ]
+    Output: 3
+
+"""
+
+from typing import List
+
+
+def numIslands(grid: List[List[str]]) -> int:
+    num = 0
+    m = len(grid)
+    n = len(grid[0])
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == "0":
+                continue
+            num += 1
+            num_of_islands_rec(i, j, m, n, grid)
+
+    return num
+
+
+def num_of_islands_rec(row, column, m, n, grid):
+    if row < 0 or row >= m:
+        return 0
+    if column < 0 or column >= n:
+        return 0
+
+    if grid[row][column] == "1":
+        grid[row][column] = "0"
+        num_of_islands_rec(row - 1, column, m, n, grid)
+        num_of_islands_rec(row, column - 1, m, n, grid)
+        num_of_islands_rec(row + 1, column, m, n, grid)
+        num_of_islands_rec(row, column + 1, m, n, grid)
+
+
+grid = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
+print(numIslands(grid))
+
+
+grid = [["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]]
+print(numIslands(grid))
