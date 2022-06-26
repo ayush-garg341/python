1a9715325f358b7e86d27a0ead1dfc9b3d70dfad
Ayush Garg <ayush.garg@innovaccer.com>
2022-06-26 22:05:25 +0530

    abstract factory pattern seen and implemented in python

---
+++ design_patterns/creational/abstract_factory/implementation.py
@@ -0,0 +1,116 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractProductA(ABC):
+    @abstractmethod
+    def useful_function_a(self):
+        pass
+
+
+class ConcreteProductA1(AbstractProductA):
+    def useful_function_a(self) -> str:
+        return "The result of the product A1."
+
+
+class ConcreteProductA2(AbstractProductA):
+    def useful_function_a(self) -> str:
+        return "The result of the product A2."
+
+
+class AbstractProductB(ABC):
+    @abstractmethod
+    def useful_function_b(self):
+        pass
+
+    @abstractmethod
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
+        pass
+
+
+class ConcreteProductB1(AbstractProductB):
+    def useful_function_b(self) -> str:
+        return "The result of the product B1."
+
+    def another_useful_function_b(self, collaborator: AbstractProductA):
+        """
+        The variant, Product B1, is only able to work correctly with the variant,
+        Product A1. Nevertheless, it accepts any instance of AbstractProductA as an
+        argument.
+        """
+        result = collaborator.useful_function_a()
+        print("The result of B1 collaborating with {}".format(result))
+
+
+class ConcreteProductB2(AbstractProductB):
+    def useful_function_b(self) -> str:
+        return "The result of the product B2."
+
+    def another_useful_function_b(self, collaborator: AbstractProductA):
+        """
+        The variant, Product B2, is only able to work correctly with the
+        variant, Product A2. Nevertheless, it accepts any instance of
+        AbstractProductA as an argument.
+        """
+        result = collaborator.useful_function_a()
+        print("The result of B2 collaborating with {}".format(result))
+
+
+class AbstractFactory(ABC):
+    """
+    The Abstract Factory interface declares a set of methods that return
+    different abstract products. These products are called a family and are
+    related by a high-level theme or concept. Products of one family are usually
+    able to collaborate among themselves. A family of products may have several
+    variants, but the products of one variant are incompatible with products of
+    another.
+    """
+
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
+
+    @abstractmethod
+    def create_product_b(self) -> AbstractProductB:
+        pass
+
+
+class ConcreteFactory1(AbstractFactory):
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA1()
+
+    def create_product_b(self) -> AbstractProductB:
+        return ConcreteProductB1()
+
+
+class ConcreteFactory2(AbstractFactory):
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA2()
+
+    def create_product_b(self) -> AbstractProductB:
+        return ConcreteProductB2()
+
+
+def client_code(factory: AbstractFactory) -> None:
+    """
+    The client code works with factories and products only through abstract
+    types: AbstractFactory and AbstractProduct. This lets you pass any factory
+    or product subclass to the client code without breaking it.
+    """
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    print(f"{product_b.useful_function_b()}")
+    print(f"{product_b.another_useful_function_b(product_a)}", end="")
+
+
+if __name__ == "__main__":
+    """
+    The client code can work with any concrete factory class.
+    """
+    print("Client: Testing client code with the first factory type:")
+    client_code(ConcreteFactory1())
+
+    print("\n")
+
+    print("Client: Testing the same client code with the second factory type:")
+    client_code(ConcreteFactory2())
