6c857bc52684789959a0cfc6e00a578e0537c4c9
Ayush Garg <ayush.garg@innovaccer.com>
2022-07-06 14:16:58 +0530

    bfs in acylic graph

---
+++ data_structures/daily_challenges/bfs_graph.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def breadthFirstSearch(self, array):
+        # Write your code here.
+        queue = []
+        queue.append(self)
+
+        while len(queue):
+            elem = queue.pop(0)
+            for child in elem.children:
+                queue.append(child)
+            array.append(elem.name)
+        return array
+
+
+graph = Node("A")
+graph.addChild("B").addChild("C").addChild("D")
+graph.children[0].addChild("E").addChild("F")
+graph.children[2].addChild("G").addChild("H")
+graph.children[0].children[1].addChild("I").addChild("J")
+graph.children[2].children[0].addChild("K")
+print(graph.breadthFirstSearch([]))
