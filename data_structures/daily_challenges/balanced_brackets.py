07ca2b0213dec06c26cc3773140564b678254825
Ayush Garg <ayush.garg@innovaccer.com>
2022-07-14 17:46:35 +0530

    balanced brackets check in a string

---
+++ data_structures/daily_challenges/balanced_brackets.py
@@ -0,0 +1,17 @@
+def balancedBrackets(string):
+    # Write your code here.
+    stack = []
+    for char in string:
+        if char == "(" or char == "[" or char == "{":
+            stack.append(char)
+        elif char == ")" or char == "]" or char == "}":
+            if stack:
+                top = stack.pop()
+                if (char == ")" and top == "(") or (char == "]" and top == "[") or (char == "}" and top == "{"):
+                    continue
+            return False
+
+    return len(stack) == 0
+
+
+print(balancedBrackets("([])(){}(())()()"))
