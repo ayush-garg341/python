66ca59bf8cbe7893536bdefbad1f2b1edb39d16f
Ayush Garg <gargayush341@gmail.com>
2022-08-25 08:52:00 +0530

    checking if two arrays represent the same bst

--- data_structures/daily_challenges/same_bsts.py
+++ data_structures/daily_challenges/same_bsts.py
@@ -22,3 +22,53 @@ def check_valid_bst_recursive(root, left_val, right_val):
     if root.val >= right_val or root.val <= left_val:
         return False
     return check_valid_bst_recursive(root.left, left_val, root.val) and check_valid_bst_recursive(root.right, root.val, right_val)
+
+
+def check_same_bst_or_not(array1, array2):
+    """
+    Arrays can have duplicates too
+    """
+    n1 = len(array1)
+    n2 = len(array2)
+
+    if n1 != n2:
+        return False
+
+    if array1[0] != array2[0]:
+        return False
+
+    return check_same_bst_or_not_rec(array1, array2)
+
+def check_same_bst_or_not_rec(arr1, arr2):
+
+    if len(arr1) != len(arr2):
+        return False
+
+    if len(arr1) == 0 or len(arr2) == 0:
+        return True
+
+    if arr1[0] != arr2[0]:
+        return False
+
+    left_tree_arr_1 = [num for num in arr1[1:] if num < arr1[0]]
+    left_tree_arr_2 = [num for num in arr2[1:] if num < arr2[0]]
+
+    right_tree_arr_1 = [num for num in arr1[1:] if num >= arr1[0]]
+    right_tree_arr_2 = [num for num in arr2[1:] if num >= arr2[0]]
+
+    return check_same_bst_or_not_rec(left_tree_arr_1, left_tree_arr_2) and check_same_bst_or_not_rec(right_tree_arr_1, right_tree_arr_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
