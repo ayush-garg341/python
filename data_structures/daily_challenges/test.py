a13b6cdea2c64f9dae30a19231d81e854433a063
Ayush Garg <gargayush341@gmail.com>
2022-09-07 11:56:23 +0530

    permutation in string using sliding window

--- data_structures/daily_challenges/test.py
+++ data_structures/daily_challenges/test.py
@@ -1,3 +1,5 @@
+# This  is test file.
+
 """
 input -> "python is my fav language"
 output -> "language fav my is python"
@@ -43,11 +45,45 @@ def reverse_words_in_string(string):
 
     return reverse_word_by_word
 
-inp = "python is my fav language"
-print(reverse_words_in_string(inp))
+# inp = "python is my fav language"
+# print(reverse_words_in_string(inp))
 
-inp2 = "I love c  "
-print(reverse_words_in_string(inp2))
+# inp2 = "I love c  "
+# print(reverse_words_in_string(inp2))
 
-inp3 = "  I hate JS  "
-print(reverse_words_in_string(inp3))
+# inp3 = "  I hate JS  "
+# print(reverse_words_in_string(inp3))
+
+
+def find_permutation(string, pattern):
+    pattern_dict = {}
+    total_match = 0
+    pattern_len = len(pattern)
+    for char in pattern:
+        if char not in pattern_dict:
+            pattern_dict[char] = 0
+        pattern_dict[char] += 1
+    window_start = 0
+    for window_end in range(len(string)):
+        char = string[window_end]
+        if char in pattern_dict:
+            pattern_dict[char] -= 1
+            if pattern_dict[char] == 0:
+                total_match += 1
+
+        if total_match == len(pattern_dict):
+            return True
+        if window_end - window_start + 1 == pattern_len:
+            start_char = string[window_start]
+            if start_char in pattern_dict:
+                if pattern_dict[start_char] == 0:
+                    total_match -= 1
+                pattern_dict[start_char] += 1
+            window_start += 1
+
+    return False
+
+print(find_permutation("eidbaooo", "ab"))
+print(find_permutation("ooolleoooleh", "hello"))
+print(find_permutation("dcda", "adc"))
+
