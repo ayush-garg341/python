44849e712133592e8aae207ce431b9ca71fd7f21
Ayush Garg <gargayush341@gmail.com>
2023-11-25 08:35:02 +0530

    generating perms using recursion

--- data_structures/subsets/permutations.py
+++ data_structures/subsets/permutations.py
@@ -52,9 +52,34 @@ def generate_permutations_recursive(nums, index, current_per, result):
             generate_permutations_recursive(nums, index + 1, new_per, result)
 
 
+def swap_char(nums, i, j):
+    new_nums = nums.copy()
+    new_nums[i], new_nums[j] = new_nums[j], new_nums[i]
+    return new_nums
+
+
+def permute_rec(nums, current_idx, result):
+    if current_idx == len(nums) - 1:
+        result.append(nums)
+        return
+
+    for i in range(current_idx, len(nums)):
+        swapped = swap_char(nums, current_idx, i)
+        permute_rec(swapped, current_idx + 1, result)
+
+
+def generate_permutations_loop(nums):
+    result = []
+    permute_rec(nums, 0, result)
+    return result
+
+
 def main():
-    print("Here are all the permutations: " + str(find_permutations([1, 3, 5])))
-    print("Here are all the permutations: " + str(generate_permutations([1, 3, 5])))
+    print("Here are all the permutations ==  " + str(find_permutations([1, 3, 5])))
+    print("Here are all the permutations == " + str(generate_permutations([1, 3, 5])))
+    print(
+        "Here are all the permutations == " + str(generate_permutations_loop([1, 3, 5]))
+    )
 
 
 main()
