778c445fe8618fe7fc9fcaab6f43466838a056ea
Ayush Garg <gargayush341@gmail.com>
2024-02-26 09:03:07 +0530

    set matrix to 0 optimized

--- data_structures/matrices/set_matrix_zero.py
+++ data_structures/matrices/set_matrix_zero.py
@@ -28,3 +28,40 @@ def mark_as_zero(row, col, mat, vset):
 
 
 print(set_matrix_zero([[1, 2, 3], [4, 5, 6], [7, 0, 9]]))
+
+
+def set_matrix_zero_optimized(mat):
+    rows = len(mat)
+    cols = len(mat[0])
+    frow = False
+    fcol = False
+    for i in range(cols):
+        if mat[0][i] == 0:
+            frow = True
+
+    for j in range(rows):
+        if mat[j][0] == 0:
+            fcol = True
+
+    for row in range(1, rows):
+        for col in range(1, cols):
+            if mat[row][col] == 0:
+                mat[row][0] = mat[0][col] = 0
+
+    for row in range(1, rows):
+        if mat[row][0] == 0:
+            for col in range(1, cols):
+                mat[row][col] = 0
+
+    for col in range(1, cols):
+        if mat[0][col] == 0:
+            for row in range(1, rows):
+                mat[row][col] = 0
+
+    if frow:
+        for col in range(cols):
+            mat[0][col] = 0
+
+    if fcol:
+        for row in range(rows):
+            mat[row][0] = 0
