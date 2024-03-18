eeb4bd2d5b80a98ea984a217296c4abf75158f09
Ayush Garg <gargayush341@gmail.com>
2024-03-18 11:05:10 +0530

    cycle detection in undirected graph using dfs

--- data_structures/graphs/cycle_undirected_graph.py
+++ data_structures/graphs/cycle_undirected_graph.py
@@ -41,7 +41,35 @@ class Solution:
 
         return False
 
+    def isCycleDFS(self, V: int, adj: List[List[int]]) -> bool:
+        visited = [False] * V
+        parent = [0] * V
+        for v in range(V):
+            if not visited[v]:
+                visited[v] = True
+                parent[v] = v
+                exist = self.isCycleDFSRec(v, adj, visited, parent)
+                if exist:
+                    return True
+        return False
 
-print(Solution().isCycle(5, [[1], [0, 2, 4], [1, 3], [2, 4], [1, 3]]))
+    def isCycleDFSRec(
+        self, v: int, adj: List[List[int]], visited: List[int], parent: List[int]
+    ) -> bool:
+        for node in adj[v]:
+            if not visited[node]:
+                visited[node] = True
+                parent[node] = v
+                ret = self.isCycleDFSRec(node, adj, visited, parent)
+                if ret:
+                    return True
+            else:
+                if parent[v] != node:
+                    return True
 
-print(Solution().isCycle(4, [[], [2], [1, 3], [2]]))
+        return False
+
+
+print(Solution().isCycleDFS(5, [[1], [0, 2, 4], [1, 3], [2, 4], [1, 3]]))
+
+print(Solution().isCycleDFS(4, [[], [2], [1, 3], [2]]))
