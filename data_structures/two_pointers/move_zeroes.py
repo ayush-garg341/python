4915cf7003f311c547fa816baf8ef5cfbb0c823e
Ayush Garg <gargayush341@gmail.com>
2026-01-08 15:15:00 +0530

    readability improved of moving zeroes

--- data_structures/two_pointers/move_zeroes.py
+++ data_structures/two_pointers/move_zeroes.py
@@ -4,11 +4,16 @@ from typing import List
 class Solution:
     def moveZeroes(self, nums: List[int]):
         # Your code goes here
+        # Check where is first 0
         first_zero = -1
         for i in range(len(nums)):
-            if nums[i] == 0 and first_zero == -1:
+            if nums[i] == 0:
                 first_zero = i
-            elif nums[i] != 0:
+                break
+
+        for i in range(len(nums)):
+            # Swap any non-zero element with the first zero index.
+            if nums[i] != 0:
                 if first_zero != -1 and i > first_zero:
                     nums[i], nums[first_zero] = nums[first_zero], nums[i]
                     first_zero += 1
